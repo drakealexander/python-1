0bb6505cab03dfad40aa067836386b6bfacdcc7f
Drake Griffith <drakegriffith8@gmail.com>
2015-07-07 19:07:15 -0400

    save

--- util.py
+++ util.py
@@ -2,13 +2,24 @@
 
 import colors as c
 
-def ask(question,color=c.yellow):
-    print(color + question + c.reset)
-    answer = input("> " + c.base3).lower().strip()
+def ask(question,color=c.yellow,lower=True,strip=True,end='\n> '):
+    print(color + question + c.reset, end=end)
+    answer = input(c.base3)
+    if lower: answer = answer.lower()
+    if strip: answer = answer.strip()
     print(c.reset)
     return answer
 
 if __name__ == '__main__':
     print(c.clear)
-    name = ask("What is your name?")
-    color = ask("What is your name in color?",c.random_color())
+    answer = ask("What is your name?")
+    print(answer)
+    answer = ask("What is your name in color?",c.random_color())
+    print(answer)
+    answer = ask("What is your full name?",lower=False)
+    print(answer)
+    answer = ask("What is something with spaces around it:")
+    print(answer)
+    answer = ask("What is something with spaces around it agian:",strip=False)
+    print(answer)
+
